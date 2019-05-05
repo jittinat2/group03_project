022f740d9584612745cc67103a4c9e3f9ffafdc6
BeseoDyz <beam583@hotmail.com>
2019-05-05 19:45:11 +0700

    B.Gen-ok! , B.page created 2

--- projectweb/src/components/BracketPage.vue
+++ projectweb/src/components/BracketPage.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="hello">
         <h1 style="font-size: 100px">{{ msg }}</h1>
-        
+        <div class="ui raised segment">
+            <div class="ui buttons" style="margin:20;">
+                <button class="ui active black blur button">Team 1</button>
+                <div class="or" data-text="VS"></div>
+                <button class="ui active orange button">Team 2</button>
+            </div>
+            <div class="ui buttons">
+                <button class="ui active black blur button">Team 1</button>
+                <div class="or" data-text="VS"></div>
+                <button class="ui active orange button">Team 2</button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "HelloWorld",   
   data() {
     return {
       msg: "Bracket Page"
