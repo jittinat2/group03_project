662896358353a452c6f6eac06e0986a9b369c9e4
jittinat <jittinoise@gmail.com>
2019-05-05 14:03:51 +0700

    first page

--- projectweb/src/App.vue
+++ projectweb/src/App.vue
@@ -1,23 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <div>
+          <!-- Just an image -->
+          <b-navbar variant="faded" type="light">
+            <b-navbar-brand href="#">
+              <img src="https://placekitten.com/g/30/30" alt="Kitten">
+            </b-navbar-brand>
+          </b-navbar>
+        </div>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template slot="button-content">
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+
+      <b-container class="bv-example-row">
+        <b-row>
+          <b-col>1 of 3</b-col>
+          <b-col cols="12" md="auto">Variable width content</b-col>
+          <b-col col lg="2">3 of 3</b-col>
+        </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 
 <script>
+import HelloWorld from "./components/HelloWorld.vue";
+
 export default {
-  name: 'App'
-}
+  name: "app",
+  components: {
+    HelloWorld
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
