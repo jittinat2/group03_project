8c7f6f8da2045b9444ffdf6e622176d1c14ab420
BeseoDyz <beam583@hotmail.com>
2019-05-05 15:37:29 +0700

    create Dropdown box

--- projectweb/src/components/BracketGen.vue
+++ projectweb/src/components/BracketGen.vue
@@ -4,7 +4,15 @@
     <!-- input type -->
     <div>
         <h1>Tourament Name :  <input type="text" v-model="tour_name"></h1>
-        <h1>Tourament Type :  <input type="text" v-model="tour_name"></h1>
+            <div>
+                <h1>Tourament Type : 
+                    <b-dropdown id="dropdown-dropright" dropright text="Drop-Right" variant="primary" class="m-5">
+                        <b-dropdown-item href="#">Action</b-dropdown-item>
+                        <b-dropdown-item href="#">Another action</b-dropdown-item>
+                        <b-dropdown-item href="#">Something else here</b-dropdown-item>
+                    </b-dropdown>
+                </h1>
+            </div>
         <h1>Tourament Size :  <input type="text" v-model="tour_name"></h1>
 
     </div>
