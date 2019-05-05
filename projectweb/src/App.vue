d7dca91265fcb8844f050492273df82f75e9aa57
BeseoDyz <beam583@hotmail.com>
2019-05-05 15:23:14 +0700

    Fix little bug

--- projectweb/src/App.vue
+++ projectweb/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
+
     <div>
       <b-navbar toggleable="lg" type="dark" variant="info">
         <div>
@@ -27,9 +27,9 @@
         </b-collapse>
       </b-navbar>
     </div>
-=======
+
     <!-- <img src="./assets/logo.png"> -->
->>>>>>> BeamBranch
+
     <router-view/>
   </div>
 </template>
