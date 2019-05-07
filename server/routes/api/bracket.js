4cb9360b8db6f1fedd853fb43307855bf6717af8
phaksuree <phaksuree2558@gmail.com>
2019-05-07 20:38:22 +0700

    bracket

--- server/routes/api/bracket.js
+++ server/routes/api/bracket.js
@@ -3,20 +3,38 @@ const mongodb = require('mongodb');
 
 const router = express.Router();
 
-// Signup
-router.post('/generator/user/:username', async (req, res) => {
+//create bracket
+router.post('/create/:username', async (req, res) => {
     const posts = await loadPostCollection();
-    await posts.findOne(
-        {username:(req.params.username)}
-    );
-    res.send (await posts.insertOne({
-        username: req.params.username,
-        bracketId: req.body.bracketId, 
-        tourName: req.body.tourName,
-        tourType: req.body.tourType,
-        size: req.body.size,
-        createdAt: new Date()
-    }));
+
+    // var max = 0
+    // var maxlenbrac = await posts.findOne({ username: ( req.params.username ) })
+
+    // for(i in maxlenbrac.bracket){
+    //     console.log((maxlenbrac.bracket[i].bracketId));
+    //     if(maxlenbrac.bracket[i].bracketId >= max){
+    //         max = maxlenbrac.bracket[i].bracketId
+    //     }
+    // }
+    // console.log("this " + max); 
+    
+    res.send( await posts.insertOne(
+        { 
+            username: req.params.username,
+            tourName: req.body.tourName,
+            tourType: req.body.tourType,
+            size: req.body.size,
+            createdAt: new Date() 
+        }
+    ))
+})
+
+//delete bracket
+router.delete('/delete/:id', async (req, res) => {
+    const posts = await loadPostCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)})
+    console.log("Deleted : " + req.params.id);
+    res.status(200).send();
 })
 
 async function loadPostCollection() {
@@ -25,7 +43,7 @@ async function loadPostCollection() {
         useNewUrlParser: true
     }) 
 
-    return client.db('vue').collection('Bracket');
+    return client.db('vue').collection('Users');
 }
 
 module.exports = router;
