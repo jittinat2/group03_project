9f4e53ee6f127c25d1293a4afd7220d49a3a42ce
jittinat <jittinoise@gmail.com>
2019-05-09 16:33:17 +0700

    Forgot Page

--- projectweb/src/components/Forgot.vue
+++ projectweb/src/components/Forgot.vue
@@ -7,11 +7,11 @@
           <form class="ui form">
             <div class="field" style="text-align: left">
               <label style="color: white; font-size: 15px">Username</label>
-              <input name="username" type="text" placeholder="Username">
+              <input name="username" type="text" v-model="forReset.username" placeholder="Username">
             </div>
             <div class="field" style="text-align: left">
               <label style="color: white; font-size: 15px">Email</label>
-              <input name="email" type="text"  placeholder="Email">
+              <input name="email" type="text"  v-model="forReset.email" placeholder="Email">
             </div>
           </form>
            <button class="ui button" type="submit" @click="Reset">Reset Password</button>
@@ -23,10 +23,39 @@
 
 <script>
 import axios from "axios";
+import router from '../router'
 export default {
   name: "HelloWorld",
   data(){
-    
+    return{
+      forReset: {
+        username:"",
+        email: ""
+      }
+    }
+  },
+  methods: {
+    Reset(){
+      if(this.forReset.username != "" && this.forReset.email != ""){
+      axios
+      .post("http://localhost:5000/profile/forget" , this.forReset)
+        .then(response => {
+          console.log(response.data.result);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      let newName = {
+        usrname: this.forReset.usrname
+      }
+      console.log(newName)
+      router.push({ name:'ResetPass', params:{newName} })
+      
+      }else
+      {
+        alert("A data must be present")
+      }
+    }
   }
 }
 </script>
